06d1a659bd14ce964bdbc19c2408b2262ff46461
wuttipat6509650716 <wuttipat.pip@dome.tu.ac.th>
2024-11-21 23:12:08 +0700

    random lucky number application

--- app/src/main/java/com/example/cs361assignment3/MainActivity.java
+++ app/src/main/java/com/example/cs361assignment3/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.cs361assignment3;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    Random random = new Random();
+    SensorManager sensorManager;
+    private static final int POLL_INTERVAL = 500;
+    private Handler hdr = new Handler();
+    Boolean shown_dialog = false;
+    SensorInfo sensor_info = new SensorInfo();
+    private static final int shake_threshold = 12;
+
+    private final Runnable pollTask = new Runnable() {
+        public void run() {
+            showDialog();
+            hdr.postDelayed(pollTask, POLL_INTERVAL);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +46,64 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+
+    }//end OnCreate
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
     }
+    public void onSensorChanged(SensorEvent event){
+        int type = event.sensor.getType();
+        if (type == Sensor.TYPE_ACCELEROMETER) {
+            sensor_info.accX=event.values[0];
+            sensor_info.accY=event.values[1];
+            sensor_info.accZ=event.values[2];
+        }
+    }
+
+    public void showDialog() {
+        if( (Math.abs(sensor_info.accX)>shake_threshold) || (Math.abs(sensor_info.accY)>shake_threshold) || (Math.abs(sensor_info.accZ)>shake_threshold) ) {
+            if(!shown_dialog) {
+                shown_dialog = true;
+                int randomInt = random.nextInt(100);
+                final AlertDialog.Builder viewDialog = new AlertDialog.Builder(this);
+                viewDialog.setIcon(android.R.drawable.dialog_frame);
+                viewDialog.setTitle(R.string.dialogtitle);
+                viewDialog.setMessage(String.valueOf(randomInt));
+
+                final TextView luckynumber = findViewById(R.id.luckynumber);
+                luckynumber.setText(String.valueOf(randomInt));
+
+                viewDialog.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                shown_dialog = false;
+                            }
+                        });
+                viewDialog.show();
+            }//end if
+        }//end if
+    }
+
+    @SuppressLint("WakelockTimeout")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        hdr.postDelayed(pollTask, POLL_INTERVAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+        hdr.removeCallbacks(pollTask);
+    }
+
+    static class SensorInfo{
+        float accX, accY, accZ;
+    }
+
 }
